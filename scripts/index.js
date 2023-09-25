f89117f05ac3bfc981bfe37d20be8b5e2672b1b9
Ilya <honeyflym@gmail.com>
2023-09-25 21:46:58 +0300

    Add script.js

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Ice Castles",
+    link: https://images.unsplash.com/photo-1695378201929-c7e68a8102bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2824&q=80
+  },
+  {
+    name: "Red stairs",
+    link: https://plus.unsplash.com/premium_photo-1691030924668-ffa1ba97d532?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80
+  },
+  {
+    name: "House on tree",
+    link: https://images.unsplash.com/photo-1547393429-098dd122091a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80
+  },
+  {
+    name: "Tower",
+    link: https://images.unsplash.com/photo-1694774433072-d6f07979e3ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80
+  },
+  {
+    name: "Night town",
+    link: https://images.unsplash.com/photo-1695249747530-57460a88ad80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80
+  },
+  {
+    name: "Canyon",
+    link: https://images.unsplash.com/photo-1505521377774-103a8cc2f735?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80
+  },
+];
