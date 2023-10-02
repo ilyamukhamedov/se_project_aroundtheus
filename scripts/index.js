b6cb465b0a73c3852376652571f9087353e32efe
Ilya <honeyflym@gmail.com>
2023-10-02 22:48:26 +0300

    Add final js

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const initialCards = [
     link: "https://plus.unsplash.com/premium_photo-1691030924668-ffa1ba97d532?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80",
   },
   {
-    name: "House on tree",
+    name: "House on a tree",
     link: "https://images.unsplash.com/photo-1547393429-098dd122091a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80",
   },
   {
@@ -28,11 +28,45 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const editModal = document.querySelector("#modal-edit");
 const modalCloseButton = document.querySelector("#modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileParagraph = document.querySelector(".profile__paragraph");
+const inputTitle = document.querySelector("#input-title");
+const inputParagraph = document.querySelector("#input-paragraph");
+const profileFormElement = editModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function closeForm() {
+  editModal.classList.remove("modal_opened");
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = inputTitle.value;
+  profileParagraph.textContent = inputParagraph.value;
+  closeForm();
+}
+
+function getCardElement(Data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = Data.name;
+  cardImage.src = Data.link;
+  cardImage.alt = Data.name;
+  return cardElement;
+}
 
 profileEditButton.addEventListener("click", () => {
+  inputTitle.value = profileTitle.textContent;
+  inputParagraph.value = profileParagraph.textContent;
   editModal.classList.add("modal_opened");
 });
 
-modalCloseButton.addEventListener("click", () => {
-  editModal.classList.remove("modal_opened");
+modalCloseButton.addEventListener("click", closeForm);
+profileFormElement.addEventListener("submit", handleFormSubmit);
+
+initialCards.forEach((Data) => {
+  const cardElement = getCardElement(Data);
+  cardList.append(cardElement);
 });
